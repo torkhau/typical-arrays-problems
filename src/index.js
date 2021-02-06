3aa504ab024f4ab464f1a7ffbc621cf758a13654
Aliaksandr Torkhau <torkhau@gmail.com>
2021-02-06 22:15:18 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0; 
+  return Math.min(...array, 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0; 
+  return Math.max(...array, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0; 
+  return array.reduce((acc, el) => acc + el, 0) / (array.length === 0 ? 1 : array.length) ;
 }
